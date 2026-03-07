6cfd40a302cf5a18d876c16fed5050ea5483e25f
lazy-coder420 <mjmunna692@gmail.com>
2026-03-08 01:40:26 +0600

    Add index.html file

--- rowjava.js
+++ rowjava.js
@@ -146,6 +146,14 @@ document.querySelectorAll(".tab").forEach(tab => {
     renderJobs();
   });
 });
+// document.querySelectorAll(".tab").forEach(tab => {
+//   tab.addEventListener("click", function(){
+//     document.querySelectorAll(".tab").forEach(t => t.classList.remove("tab-active"));
+//     this.classList.add("tab-active");
+//     currentTab = this.dataset.tab;
+//     renderJobs();
+//   });
+// });
 
 renderJobs();
 
